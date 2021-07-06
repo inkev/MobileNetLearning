60fb0ea573a63d1e8f748df994cf5493d0712243
kevin <inkev@students.cs.ubc.ca>
2021-07-06 13:18:01 -0700

    uploading images

--- sketch.js
+++ sketch.js
@@ -1,11 +1,28 @@
 let mobilenet;
+let cat;
 
 function modelReady() {
 	console.log('Model is ready!!');
+	mobilenet.predict(cat, gotResults);
 }
+
+function gotResults(error, results) {
+	if (error) {
+		console.error(error);
+	} else {
+		console.log(results);
+	}
+}
+
+function imageReady() {
+	image(cat, 0, 0, width, height)
+}
+
 function setup() {
 	createCanvas(640, 480);
 	background(0);
+	cat = createImg('images/cat.jpg', imageReady);
+	cat.hide();
 
 	mobilenet = ml5.imageClassifier('MobileNet', modelReady);
 }
